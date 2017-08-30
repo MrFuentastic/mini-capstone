ae00ef11992c56adb9601b7d8b31cd53a970a464
MrFuentastic <vfuentes8587@gmail.com>
2017-08-29 21:12:11 -0500

    added a product page

--- app/controllers/organs_controller.rb
+++ app/controllers/organs_controller.rb
@@ -1,2 +1,6 @@
 class OrgansController < ApplicationController
+  def organs_action
+    @organs = Organ.all
+    render 'organs_page.html.erb'
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+organ = Organ.new(
+                  name: 'Heart',
+                  price: 456.88,
+                  image: 'https://upload.wikimedia.org/wikipedia/en/0/0b/Heart_album_cover.jpg',
+                  description: 'The self titled 1985 album from, you guessed it, Heart',
+                  )
+
+organ.save
+
+organ = Organ.new(
+                  name: 'Einsteins Brain',
+                  price: 100000.00,
+                  image: 'http://i.dailymail.co.uk/i/pix/2015/05/03/19/2847FC5F00000578-3066475-image-a-60_1430678352628.jpg',
+                  description: 'A collectors item, originally donated to science, now you can have it on your living room display shelf!',
+                  )
+
+organ.save
+
+organ = Organ.new(
+                  name: 'Lung',
+                  price: 0.50,
+                  image: 'http://www.smokerslungs.org/wp-content/uploads/2009/11/smokers-lung-picture-3.png',
+                  description: "On clearance, it was donated by the Lung Cancer Association of America, was a smoker's lung used in D.A.R.E presentations.",
+                  )
+
+organ.save
+
+organ = Organ.new(
+                  name: 'Kidney',
+                  price: 3000.00,
+                  image: 'http://www.medicalgrapevineasia.com/mg/wp-content/uploads/2015/04/Donor-Kidney-on-Ice-before-transplantation.jpg',
+                  description: "Recently aquired through our... business partners. Still fresh! Hurry before it's no longer viable!",
+                  )
+
+organ.save
+
