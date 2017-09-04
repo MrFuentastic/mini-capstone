1dd6e230f418ac27901c881e28e53e0393122d5f
MrFuentastic <vfuentes8587@gmail.com>
2017-09-04 15:09:12 -0500

    added a form to add new items, along with the routes.

--- app/controllers/organs_controller.rb
+++ app/controllers/organs_controller.rb
@@ -3,6 +3,19 @@ class OrgansController < ApplicationController
     @organs = Organ.all
   end
 
+  def new
+  end
+
+  def create
+    organ = Organ.new(
+                      name: params[:name],
+                      price: params[:price],
+                      image: params[:image],
+                      description: params[:description]
+                      )
+    organ.save
+  end
+
   def show
     @organ = Organ.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/organs' => 'organs#index'
+  get '/organs/new' => 'organs#new'
+  post '/organs' => 'organs#create'
   get '/organs/:id' => 'organs#show'
 end
