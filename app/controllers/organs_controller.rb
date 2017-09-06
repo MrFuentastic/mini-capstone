a84c68e936370ba7f491a5c71c526e060b66aabf
MrFuentastic <vfuentes8587@gmail.com>
2017-09-05 20:44:52 -0500

    added routes, deleted unnecesary view pages

--- app/controllers/organs_controller.rb
+++ app/controllers/organs_controller.rb
@@ -14,10 +14,33 @@ class OrgansController < ApplicationController
                       description: params[:description]
                       )
     organ.save
+    redirect_to "/organs/#{organ.id}"
   end
 
   def show
     @organ = Organ.find(params[:id])
+  end
+
+  def edit
+    @organ = Organ.find(params[:id])
+  end
+
+  def update
+    organ = Organ.find(params[:id])
+    organ.assign_attributes(
+                              name: params[:name],
+                              price: params[:price],
+                              image: params[:image],
+                              description: params[:description]
+                              )
+    organ.save
+    redirect_to "/organs/#{organ.id}"
+  end
+
+  def destroy
+    organ = Organ.find(params[:id])
+    organ.destroy
+    redirect_to "/organs/"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get '/organs/new' => 'organs#new'
   post '/organs' => 'organs#create'
   get '/organs/:id' => 'organs#show'
+  get '/organs/:id/edit' => 'organs#edit'
+  patch '/organs/:id' => 'organs#update'
+  delete '/organs/:id' => 'organs#destroy'
 end
